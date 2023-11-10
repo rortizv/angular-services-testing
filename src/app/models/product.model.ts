844fdcddcb746672489ef76a9e2238a44cb4ed62
Rafael Ortiz <rortiz@inkremental.co>
2023-11-09 19:51:41 -0500

    Display grid

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -2,7 +2,7 @@ import { Category } from "./category.model";
 
 export interface Product {
   id: number;
-  name: string;
+  title: string;
   price: number;
   description: string;
   images: string;
